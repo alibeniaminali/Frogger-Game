f6b1066d906c815e35d15ef7d130bb55c5d2de36
Ali Ali <alibeniaminali@gmail.com>
2022-01-14 13:30:50 +0000

    deleted previous functions that weren't working and made the woods inside of the second river to move

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ function init() {
   let frogCurrentPosition = 98
   const lilyClass = 'lily'
   const lilyPostition = 1
-  let slowCar
   const slowCars = []
   const fastCars = []
   const slowWoods = []
@@ -63,10 +62,14 @@ function init() {
   }
   document.addEventListener('keyup', handleKeyUp)
 
-  // * Create river
+  // * Create river and wood to be able to move later
   const river2 = cells.slice(20, 30)
-  river2.forEach((element) => {
+  river2.forEach((element, i) => {
     element.classList.add('river')
+    if (i === 2 || i === 5 || i === 8) {
+      element.classList.add('wood')
+      fastWoods.push(element)
+    }
   })
   const river1 = cells.slice(40, 50)
   river1.forEach((element, i) => {
@@ -76,7 +79,7 @@ function init() {
       slowWoods.push(element)
     }
   })
-  // * Create road
+  // * Create road and car to be able to move later
   const road2 = cells.slice(60, 70)
   road2.forEach((element, i) => {
     element.classList.add('road')
@@ -100,29 +103,6 @@ function init() {
   fastCars.forEach((element) => {
     element.classList.add('car1')
   })
-
-  // * Create and position the woods
-  // const wood1of2 = document.createElement('div')
-  // river1[0].appendChild(wood1of2)
-  // const wood2of2 = document.createElement('div')
-  // river1[1].appendChild(wood2of2)
-  // const wood1of22 = document.createElement('div')
-  // river1[6].appendChild(wood1of22)
-  // const wood2of22 = document.createElement('div')
-  // river1[7].appendChild(wood2of22)
-
-  // // * Fast woods
-  // const fastWood1of2 = document.createElement('div')
-  // river2[8].appendChild(fastWood1of2)
-  // const fastWood2of2 = document.createElement('div')
-  // river2[9].appendChild(fastWood2of2)
-  // const fastWood1of22 = document.createElement('div')
-  // river2[3].appendChild(fastWood1of22)
-  // const fastWood2of22 = document.createElement('div')
-  // river2[4].appendChild(fastWood2of22)
-
-  // const slowWoods = [wood1of2, wood2of2, wood1of22, wood2of22]
-  // const fastWoods = [fastWood1of2, fastWood2of2, fastWood1of22, fastWood2of22]
 
   slowWoods.forEach((element) => {
     element.classList.add('wood')
@@ -178,38 +158,17 @@ function init() {
   }, 300)
 
   fastWood = setInterval(() => {
-    fastWoods.forEach((wood) => {
-      const riverPosition = river2.indexOf(wood.parentElement)
-      river2[riverPosition].removeChild(wood)
+    fastWoods.forEach((wood, i) => {
+      const riverPosition = river2.indexOf(wood)
+      river2[riverPosition].classList.remove('wood')
       if (riverPosition === 0) {
-        river2[9].appendChild(wood)
+        fastWoods[i] = river2[9]
       } else {
-        river2[riverPosition - 1].appendChild(wood)
+        fastWoods[i] = river2[riverPosition - 1]
       }
+      fastWoods[i].classList.add('wood')
     })
   }, 300)
-
-  // * Win or Lose Functions
-  // function winOrLose() {
-  //   if (cells[1].classList.contains('frog')) {
-  //     result.innerHTML = 'You Won!'
-  //     result.style.color = 'green'
-  //     result.style.fontSize = '50px'
-  //     result.style.padding = '2px'
-  //     removeFrog(frogCurrentPosition)
-  //     frogCurrentPosition = frogStartPosition
-  //     addFrog(frogStartPosition)
-  //   } else if (
-  //     cells[frogCurrentPosition].classList.contains('car') ||
-  //     cells[frogCurrentPosition].classList.contains('river')
-  //   ) {
-  //     result.innerHTML = 'You Lost!'
-  //     result.style.color = 'red'
-  //     removeFrog(frogCurrentPosition)
-  //     frogCurrentPosition = frogStartPosition
-  //     addFrog(frogStartPosition)
-  //   }
-  // }
 
   function winOrLose() {
     if (cells[1].classList.contains('frog')) {
@@ -222,8 +181,7 @@ function init() {
       addFrog(frogStartPosition)
     } else if (
       cells[frogCurrentPosition].classList.contains('car') ||
-      cells[frogCurrentPosition].classList.contains('car1') ||
-      cells[frogCurrentPosition].classList.contains('wood')
+      cells[frogCurrentPosition].classList.contains('car1')
     ) {
       result.innerHTML = 'You Lost!'
       result.style.color = 'red'
